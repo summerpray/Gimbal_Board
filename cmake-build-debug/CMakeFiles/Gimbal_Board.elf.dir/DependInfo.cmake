c951c045313757d95142535f406166448251f700
summerpray <1141430447@qq.com>
2021-11-03 12:00:29 +0800

    start project

---
+++ cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/DependInfo.cmake
@@ -0,0 +1,138 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/worker/CLionProjects/Gimbal_Board/startup/startup_stm32f407xx.s" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_crc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pcd_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rng.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rng.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rtc_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src/usbd_cdc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/list.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/adc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/adc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/can.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/can.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/crc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/crc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/dma.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/dma.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/freertos.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/freertos.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/gpio.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/gpio.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/i2c.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/i2c.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/main.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/main.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/rng.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/rng.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/rtc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/rtc.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/spi.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/spi.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/stm32f4xx_hal_msp.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/stm32f4xx_it.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/stm32f4xx_it.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/syscalls.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/syscalls.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/system_stm32f4xx.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/system_stm32f4xx.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/tim.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/tim.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/usart.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/usart.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/usb_device.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/usb_device.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/usbd_cdc_if.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/usbd_cdc_if.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/usbd_conf.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/usbd_conf.c.obj"
+  "C:/Users/worker/CLionProjects/Gimbal_Board/Src/usbd_desc.c" "C:/Users/worker/CLionProjects/Gimbal_Board/cmake-build-debug/CMakeFiles/Gimbal_Board.elf.dir/Src/usbd_desc.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "../Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
